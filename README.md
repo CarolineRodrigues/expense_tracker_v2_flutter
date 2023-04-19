c8823f7229704a5bf8757aeba21d76d187099702
CarolineCRodrigues <caroline.rodrigues@ticapp.gov.pt>
2023-04-19 23:05:58 +0100

    Expenses Tracker

---
+++ README.md
@@ -0,0 +1,3 @@
+# expense_tracker_v2_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/expense_tracker_v2_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expense_tracker_v2_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
